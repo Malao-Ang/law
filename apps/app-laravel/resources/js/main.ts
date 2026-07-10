0279fb049e222fe5071b20c5c6c69a68b5c24ecd
MALAO <kai84143@gmail.com>
2026-07-10 22:14:04 +0700

    chore(report): add vue3-apexcharts dependency

--- apps/app-laravel/resources/js/main.ts
+++ apps/app-laravel/resources/js/main.ts
@@ -1,6 +1,7 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import VueApexCharts from 'vue3-apexcharts';
 import App from './App.vue';
 import { router } from './router';
 import { vuetify } from './plugins/vuetify';
@@ -9,4 +10,5 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(vuetify);
+app.use(VueApexCharts);
 app.mount('#app');
